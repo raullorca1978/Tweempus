51456b2fd3c151fe02e1be0100acfa3775df7182
raul <raul.lorca@gmail.com>
2024-08-28 19:10:13 +0200

    Se añade el servicio que devuelve author

--- Tweempus/src/app/dashboard/dashboard.component.ts
+++ Tweempus/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorService } from '../shared/author/author.service';
 
 @Component({
   selector: 'tweempus-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent  implements OnInit{
+  constructor(
+    private authorService: AuthorService
+
+  ){}
+
+  ngOnInit(){
+    this.authorService.getAuthor(`1`).subscribe(author => console.log(author))
+  }
 
 }
--- Tweempus/src/app/shared/author/author.service.ts
+++ Tweempus/src/app/shared/author/author.service.ts
@@ -1,12 +1,39 @@
 import { Injectable } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
-import { Observable,catchError, map } from 'rxjs';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { Observable,catchError, map, throwError } from 'rxjs';
+import { Author } from './author.model';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorService {
+  private url: string= 'http://localhost:3000/authors';
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getAuthor(id: string):Observable<any>{
+    let author: Author = new Author(id);
+    //let author: Author = null; //asi es en el curso
+    return this.httpClient.get<Author>(this.url + '/'+ id).pipe(
+      map(dbAuthor =>{
+        author= new Author(dbAuthor.id);
+        author.fullName = dbAuthor.fullName;
+        author.image = dbAuthor.image;
+        author.url ='http://localhost:4200/author/' + dbAuthor; 
+        return author
+      }),
+      catchError(this.handleError)
+    );
+
+  }
+
+  handleError(error: any){
+    let erroMsg = (error.message) ? error.message:
+    error.status? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(erroMsg);
+    return  throwError(()=> erroMsg)
+  }
 }
